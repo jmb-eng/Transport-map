15343f4ea9428ad80fa360a721ace6ef4c5a33fa
jmb-eng <joavabelleza.srdp@gmail.com>
2026-04-29 15:04:14 +0800

    Update apps.js

--- apps.js
+++ apps.js
@@ -365,7 +365,7 @@ north.addTo(map);
         background: rgba(255,255,255,0.8);
         padding: 10px;
         border-radius: 8px;
-        width: 700px;
+        width: 550px;
         max-height: 300px;
         overflow-y: auto;
       ">
